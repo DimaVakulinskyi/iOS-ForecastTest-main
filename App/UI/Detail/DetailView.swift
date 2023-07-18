73fc64e49720ea18ebd74bbc736df666ab332438
Dmytro Vakulinskyi <d.vakulinskyi@gmail.com>
2023-07-18 11:27:08 +0300

    Moved view code to detailView

--- App/UI/Detail/DetailView.swift
+++ App/UI/Detail/DetailView.swift
@@ -8,5 +8,94 @@ import UIKit
 
 class DetailView: UIView {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .white
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let highLowLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let sunriseSunsetLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let chanceRainLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        addSubview(imageView)
+        addSubview(descriptionLabel)
+        addSubview(highLowLabel)
+        addSubview(sunriseSunsetLabel)
+        addSubview(chanceRainLabel)
+
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 100),
+            imageView.heightAnchor.constraint(equalToConstant: 100),
+
+            descriptionLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            descriptionLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+            highLowLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            highLowLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+            sunriseSunsetLabel.topAnchor.constraint(equalTo: highLowLabel.bottomAnchor, constant: 10),
+            sunriseSunsetLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+            chanceRainLabel.topAnchor.constraint(equalTo: sunriseSunsetLabel.bottomAnchor, constant: 10),
+            chanceRainLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+    }
+
+    func configure(with forecastItem: ForecastItem) {
+        imageView.image = UIImage(named: forecastItem.type)
+        descriptionLabel.text = forecastItem.description
+        highLowLabel.text = "High: \(forecastItem.high)°, Low: \(forecastItem.low)°"
+        sunriseSunsetLabel.text = "Sunrise: \(forecastItem.sunrise), Sunset: \(forecastItem.sunset)"
+        chanceRainLabel.text = "Chance of Rain: \(forecastItem.chanceRain)%"
+    }
 }
+
 
--- App/UI/Detail/DetailViewController.swift
+++ App/UI/Detail/DetailViewController.swift
@@ -11,51 +11,7 @@ import GlobalUI
 class DetailViewController: UIViewController {
 
     private var viewModel: DetailViewModel
-
-    private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .white
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-
-    private let descriptionLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    private let highLowLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    private let sunriseSunsetLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    private let chanceRainLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    private var detailView: DetailView!
 
     public init(viewModel: DetailViewModel) {
         self.viewModel = viewModel
@@ -68,47 +24,17 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationController?.navigationBar.tintColor = .white
         view.backgroundColor = GlobalColor.backgroundColor
 
-        view.addSubview(imageView)
-        view.addSubview(descriptionLabel)
-        view.addSubview(highLowLabel)
-        view.addSubview(sunriseSunsetLabel)
-        view.addSubview(chanceRainLabel)
-
-        setupLayout()
+        detailView = DetailView()
+        detailView.center = self.view.center
+        view.addSubview(detailView)
         load()
     }
 
-    private func setupLayout() {
-        NSLayoutConstraint.activate([
-            
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: 100),
-            imageView.heightAnchor.constraint(equalToConstant: 100),
-
-            descriptionLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
-            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-            highLowLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
-            highLowLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-            sunriseSunsetLabel.topAnchor.constraint(equalTo: highLowLabel.bottomAnchor, constant: 10),
-            sunriseSunsetLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-            chanceRainLabel.topAnchor.constraint(equalTo: sunriseSunsetLabel.bottomAnchor, constant: 10),
-            chanceRainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        ])
-    }
-
     private func load() {
-        imageView.image = UIImage(named: viewModel.forecastItem.type)
-        descriptionLabel.text = viewModel.forecastItem.description
-        highLowLabel.text = "High: \(viewModel.forecastItem.high)°, Low: \(viewModel.forecastItem.low)°"
-        sunriseSunsetLabel.text = "Sunrise: \(viewModel.forecastItem.sunrise), Sunset: \(viewModel.forecastItem.sunset)"
-        chanceRainLabel.text = "Chance of Rain: \(viewModel.forecastItem.chanceRain)%"
+        detailView.configure(with: viewModel.forecastItem)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
